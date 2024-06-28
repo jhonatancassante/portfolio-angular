103b9db9d8012451f5414ad243d22fc5c7d6fe2c
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2024-06-28 10:48:10 -0300

    feat: implemented projects page

--- src/app/pages/projetos/projetos.component.ts
+++ src/app/pages/projetos/projetos.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-projetos',
-  standalone: true,
-  imports: [],
-  templateUrl: './projetos.component.html',
-  styleUrl: './projetos.component.scss'
+    selector: 'app-projetos',
+    standalone: true,
+    imports: [],
+    templateUrl: './projetos.component.html',
+    styleUrl: './projetos.component.scss',
 })
 export class ProjetosComponent {
+    menuOptions: string[] = ['pokedex', 'dost on screen', 'formulário'];
+    menuIsVisible: boolean = true;
 
+    toggleMenuVisibility(): void {
+        this.menuIsVisible = !this.menuIsVisible;
+        console.log(this.menuIsVisible);
+    }
+
+    closeMenu(): void {
+        this.menuIsVisible = false;
+    }
 }
